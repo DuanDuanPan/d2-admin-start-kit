254b1ca5a5b7a169b865316c07f12c584e451d80
潘端端 <enjoyjavapan@163.com>
2020-10-12 16:22:45 +0800

    1、格式设置 2、登录删减 3、首页删减

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,33 @@
+/*
+ * @Date: 2020-10-12 14:19:11
+ * @Author: enjoyjavapan
+ * @LastEditors: enjoyjavapan
+ * @LastEditTime: 2020-10-12 15:20:23
+ * @Description: 描述文件作用
+ * @FilePath: \d2-admin-start-kit\.eslintrc.js
+ */
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  overrides: [
-    {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)'
-      ],
-      env: {
-        jest: true
-      }
-    }
-  ]
+    root: true,
+    env: {
+        node: true
+    },
+    extends: ['plugin:vue/essential', '@vue/standard'],
+    rules: {
+        indent: ['off', 4],
+        'space-before-function-paren': ['off', 0],
+        'object-curly-spacing': ['off', 0],
+        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    },
+    parserOptions: {
+        parser: 'babel-eslint'
+    },
+    overrides: [
+        {
+            files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
+            env: {
+                jest: true
+            }
+        }
+    ]
 }
--- src/libs/util.js
+++ src/libs/util.js
@@ -1,34 +1,42 @@
+/*
+ * @Date: 2020-10-12 14:19:11
+ * @Author: enjoyjavapan
+ * @LastEditors: enjoyjavapan
+ * @LastEditTime: 2020-10-12 15:21:51
+ * @Description: 描述文件作用
+ * @FilePath: \d2-admin-start-kit\src\libs\util.js
+ */
 import cookies from './util.cookies'
 import db from './util.db'
 import log from './util.log'
 
 const util = {
-  cookies,
-  db,
-  log
+    cookies,
+    db,
+    log
 }
 
 /**
  * @description 更新标题
  * @param {String} title 标题
  */
-util.title = function (titleText) {
-  const processTitle = process.env.VUE_APP_TITLE || 'D2Admin'
-  window.document.title = `${processTitle}${titleText ? ` | ${titleText}` : ''}`
+util.title = function(titleText) {
+    const processTitle = process.env.VUE_APP_TITLE || '奥兰托协同研发平台'
+    window.document.title = `${processTitle}${titleText ? ` | ${titleText}` : ''}`
 }
 
 /**
  * @description 打开新页面
  * @param {String} url 地址
  */
-util.open = function (url) {
-  var a = document.createElement('a')
-  a.setAttribute('href', url)
-  a.setAttribute('target', '_blank')
-  a.setAttribute('id', 'd2admin-link-temp')
-  document.body.appendChild(a)
-  a.click()
-  document.body.removeChild(document.getElementById('d2admin-link-temp'))
+util.open = function(url) {
+    var a = document.createElement('a')
+    a.setAttribute('href', url)
+    a.setAttribute('target', '_blank')
+    a.setAttribute('id', 'd2admin-link-temp')
+    document.body.appendChild(a)
+    a.click()
+    document.body.removeChild(document.getElementById('d2admin-link-temp'))
 }
 
 export default util
