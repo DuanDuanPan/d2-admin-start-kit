77b372b1c7ecb4faf6f1d845ed7ffadeccaae4e9
潘端端 <enjoyjavapan@163.com>
2020-10-12 17:23:52 +0800

    1、删除测试代码

--- src/menu/index.js
+++ src/menu/index.js
@@ -1,42 +1,44 @@
-import { uniqueId } from 'lodash'
+/*
+ * @Date: 2020-10-12 14:19:11
+ * @Author: enjoyjavapan
+ * @LastEditors: enjoyjavapan
+ * @LastEditTime: 2020-10-12 16:48:46
+ * @Description: 描述文件作用
+ * @FilePath: \d2-admin-start-kit\src\menu\index.js
+ */
+import {uniqueId} from 'lodash'
 
 /**
  * @description 给菜单数据补充上 path 字段
  * @description https://github.com/d2-projects/d2-admin/issues/209
  * @param {Array} menu 原始的菜单数据
  */
-function supplementPath (menu) {
-  return menu.map(e => ({
-    ...e,
-    path: e.path || uniqueId('d2-menu-empty-'),
-    ...e.children ? {
-      children: supplementPath(e.children)
-    } : {}
-  }))
+function supplementPath(menu) {
+    return menu.map(e => ({
+        ...e,
+        path: e.path || uniqueId('d2-menu-empty-'),
+        ...(e.children
+            ? {
+                  children: supplementPath(e.children)
+              }
+            : {})
+    }))
 }
 
 export const menuHeader = supplementPath([
-  { path: '/index', title: '首页', icon: 'home' },
-  {
-    title: '页面',
-    icon: 'folder-o',
-    children: [
-      { path: '/page1', title: '页面 1' },
-      { path: '/page2', title: '页面 2' },
-      { path: '/page3', title: '页面 3' }
-    ]
-  }
+    {path: '/index', title: '首页', icon: 'home'},
+    {
+        title: '系统管理',
+        icon: 'folder-o',
+        children: [{path: '/sysmgr-usermgr', title: '用户管理'}]
+    }
 ])
 
 export const menuAside = supplementPath([
-  { path: '/index', title: '首页', icon: 'home' },
-  {
-    title: '页面',
-    icon: 'folder-o',
-    children: [
-      { path: '/page1', title: '页面 1' },
-      { path: '/page2', title: '页面 2' },
-      { path: '/page3', title: '页面 3' }
-    ]
-  }
+    {path: '/index', title: '首页', icon: 'home'},
+    {
+        title: '系统管理',
+        icon: 'folder-o',
+        children: [{path: '/sysmgr-usermgr', title: '用户管理'}]
+    }
 ])
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,3 +1,11 @@
+/*
+ * @Date: 2020-10-12 14:19:11
+ * @Author: enjoyjavapan
+ * @LastEditors: enjoyjavapan
+ * @LastEditTime: 2020-10-12 16:49:05
+ * @Description: 描述文件作用
+ * @FilePath: \d2-admin-start-kit\src\router\routes.js
+ */
 import layoutHeaderAside from '@/layout/header-aside'
 
 // 由于懒加载页面太多的话会造成webpack热更新太慢，所以开发环境不使用懒加载，只有生产环境使用懒加载
@@ -7,105 +15,82 @@ const _import = require('@/libs/util.import.' + process.env.NODE_ENV)
  * 在主框架内显示
  */
 const frameIn = [
-  {
-    path: '/',
-    redirect: { name: 'index' },
-    component: layoutHeaderAside,
-    children: [
-      // 首页
-      {
-        path: 'index',
-        name: 'index',
-        meta: {
-          auth: true
-        },
-        component: _import('system/index')
-      },
-      // 演示页面
-      {
-        path: 'page1',
-        name: 'page1',
-        meta: {
-          title: '页面 1',
-          auth: true
-        },
-        component: _import('demo/page1')
-      },
-      {
-        path: 'page2',
-        name: 'page2',
-        meta: {
-          title: '页面 2',
-          auth: true
-        },
-        component: _import('demo/page2')
-      },
-      {
-        path: 'page3',
-        name: 'page3',
-        meta: {
-          title: '页面 3',
-          auth: true
-        },
-        component: _import('demo/page3')
-      },
-      // 系统 前端日志
-      {
-        path: 'log',
-        name: 'log',
-        meta: {
-          title: '前端日志',
-          auth: true
-        },
-        component: _import('system/log')
-      },
-      // 刷新页面 必须保留
-      {
-        path: 'refresh',
-        name: 'refresh',
-        hidden: true,
-        component: _import('system/function/refresh')
-      },
-      // 页面重定向 必须保留
-      {
-        path: 'redirect/:route*',
-        name: 'redirect',
-        hidden: true,
-        component: _import('system/function/redirect')
-      }
-    ]
-  }
+    {
+        path: '/',
+        redirect: {name: 'index'},
+        component: layoutHeaderAside,
+        children: [
+            // 首页
+            {
+                path: 'index',
+                name: 'index',
+                meta: {
+                    auth: true
+                },
+                component: _import('system/index')
+            },
+            {
+                path: '/sysmgr-usermgr',
+                name: 'sysmgr-usermgr',
+                meta: {
+                    title: '用户管理',
+                    auth: true
+                },
+                component: _import('sysmgr/usermgr')
+            },
+            // 系统 前端日志
+            {
+                path: 'log',
+                name: 'log',
+                meta: {
+                    title: '前端日志',
+                    auth: true
+                },
+                component: _import('system/log')
+            },
+            // 刷新页面 必须保留
+            {
+                path: 'refresh',
+                name: 'refresh',
+                hidden: true,
+                component: _import('system/function/refresh')
+            },
+            // 页面重定向 必须保留
+            {
+                path: 'redirect/:route*',
+                name: 'redirect',
+                hidden: true,
+                component: _import('system/function/redirect')
+            }
+        ]
+    }
 ]
 
 /**
  * 在主框架之外显示
  */
 const frameOut = [
-  // 登录
-  {
-    path: '/login',
-    name: 'login',
-    component: _import('system/login')
-  }
+    // 登录
+    {
+        path: '/login',
+        name: 'login',
+        component: _import('system/login')
+    }
 ]
 
 /**
  * 错误页面
  */
 const errorPage = [
-  {
-    path: '*',
-    name: '404',
-    component: _import('system/error/404')
-  }
+    {
+        path: '*',
+        name: '404',
+        component: _import('system/error/404')
+    }
 ]
 
 // 导出需要显示菜单的
 export const frameInRoutes = frameIn
 
 // 重新组织后导出
-export default [
-  ...frameIn,
-  ...frameOut,
-  ...errorPage
-]
+export default [...frameIn, ...frameOut, ...errorPage]
